0ca770ccd11a90314fca7ef0bb5c214325d6aee2
Fahimullah-67 <179002904+fahimullah-67@users.noreply.github.com.>
2026-04-18 23:21:59 +0500

    Create Embedding model use Gemini api... simple query

---
+++ EmbeddedModel/1_embedding_gemini_query.py
@@ -0,0 +1,11 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
+
+result = embedding.embed_query("What is the capital of France?")
+
+print(result)
+
