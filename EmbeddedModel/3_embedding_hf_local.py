f59bca7ba58e871af4744f25dda20c6ac2f56aef
Fahimullah-67 <179002904+fahimullah-67@users.noreply.github.com.>
2026-04-19 00:00:59 +0500

    Add embedding example using HuggingFace model for query processing

---
+++ EmbeddedModel/3_embedding_hf_local.py
@@ -0,0 +1,9 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+text= "What is the capital of Pakistan?"
+
+vector = embedding.embed_query(text)
+
+print(str(vector))
